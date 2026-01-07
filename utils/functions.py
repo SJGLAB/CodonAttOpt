37c4983b534aa443e43dfacccd1045cf0caf296f
NVSI_SJGLAB <115686053+SJGLAB@users.noreply.github.com>
2026-01-07 13:28:23 +0800

    Update functions.py

--- utils/functions.py
+++ utils/functions.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
-# @Author: Jie
-# @Date:   2017-06-14 17:34:32
-# @Last Modified by:   Jie Yang,     Contact: jieynlp@gmail.com
-# @Last Modified time: 2018-06-22 00:01:47
+# Code adapted from: https://github.com/jiesutd/NCRFpp
+# Original author: Yang, Jie and Zhang, Yue
+
 from __future__ import print_function
 from __future__ import absolute_import
 import sys
@@ -377,3 +375,4 @@ if __name__ == '__main__':
     print(norm2one(a))
 
 2
+
